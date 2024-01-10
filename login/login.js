547a58cf4f4d4949cb57a06cbb6dfbb924208656
Yeah-metcha-Someday <itowokashigenari.jobjobworking@gmail.com>
2024-01-10 22:52:03 +0900

    特定のemailとpassでmypage.htmlを開く

--- login/login.js
+++ login/login.js
@@ -1,6 +1,27 @@
 'use strict';
 
-document.getElementById('form').onsubmit = function() {
-  const entered_email = document.getElementById('form').email.value
-  console.log(entered_email)
-};
+const button = document.getElementById('button');
+
+// ボタンクリック時に入力されたemailとpasswordを取得
+button.addEventListener('click', function(){
+  const searchEmail = document.querySelector('#email').value;
+  const searchPass = document.querySelector('#password').value;
+  setCookie(searchEmail);
+  login(searchEmail, searchPass);
+});
+
+// emailとpasswordのチェック
+function login(email, pass) {
+  if(email === 'tetsuya@example.com' && pass === 'hogehuga') {
+    window.open('mypage.html'); // mypage.htmlを開く
+  } else {
+    alert('メールアドレスまたはパスワードが異なります。');
+  }
+}
+
+function setCookie(email) {
+  if (navigator.cookieEnabled) {
+    document.cookie = "id=123"  // + email;
+    alert(document.cookie);
+  }
+}
